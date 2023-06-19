0e1c3a760d8f2ea6ef4e07c000252de185681c1a
Tales Panoutsos <tales.panoutsos@gmail.com>
2023-06-19 01:25:45 -0300

    Adcionando tamanho aos pontos do mapa

--- mapScript.js
+++ mapScript.js
@@ -64,35 +64,39 @@ function loadMap() {
     fenomenoSeries = map.choropleth(getStatesDataSets());
     fenomenoSeries.labels().fontColor('black');
   
-    fireSpotsDataset = anychart.data.set(fireSpots).mapAs();
-    let min = 1
-    let max = 580
-    mapSeries = map.marker(fireSpotsDataset.filter('year', filterByYear));
+    fireSpotsDataset = anychart.data.set(fireSpots).mapAs({size: "firespots"});
+
+    let min = 1;
+    let max = 580;
+    mapSeries = map.bubble(fireSpotsDataset.filter('year', filterByYear));
+    
     mapSeries
         .name('Focos de Incêndio')
         .fill((el) => {
             if (el.index >= 0) {
-                const { firespots } = el.iterator.f
-                return `rgb(${scale(firespots, min, max, 140, 255)}, 0, 0)`
+                const { firespots } = el.iterator.f;
+                return `rgba(${scale(firespots, min, max, 140, 255)}, 0, 0, 0.3)`;
             }
-            return 'red'
+            return 'red';
         })
         .stroke((el) => {
             if (el.index >= 0) {
-                const { firespots } = el.iterator.f
-                return `rgb(${scale(firespots, min, max, 140, 255)}, 0, 0)`
+                const { firespots } = el.iterator.f;
+                return `rgb(${scale(firespots, min, max, 140, 255)}, 0, 0)`;
             }
-            return 'red'
+            return 'red';
         })
         .type('circle')
-        .size(4)
         .labels(false)
         .selectionMode('none');
+    
+    map.minBubbleSize("1%");
+    map.maxBubbleSize("5%");
 
     mapSeries.hovered().stroke('2 #fff').size(8);
 
     // sets Tooltip for series
-    mapSeries.tooltip().titleFormat("Coordenadas")
+    mapSeries.tooltip().titleFormat("Coordenadas");
 
     mapSeries
         .legendItem()
@@ -108,4 +112,4 @@ function loadMap() {
   
     //initiate map drawing
     map.draw();
-  };
+}
